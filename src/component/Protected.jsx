182989da861b4b1ef0d15d71fbf80a72fe2c7729
Rupam1210 <guptarupam548@gmail.com>
2025-05-06 17:29:04 +0530

    user

--- src/component/Protected.jsx
+++ src/component/Protected.jsx
@@ -3,14 +3,17 @@
 // import { toast } from 'react-toastify'
 import { Navigate } from 'react-router-dom'
 import { useCookies } from 'react-cookie';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
  
  
 
 const Protected = ({children}) => {
+    const { user } = useContext(UserContext);
    const[cookie,setcookie]=useCookies(['token'])
     
-    if(cookie.token){ 
+    if(user){ 
         return children
     } else{
         return <Navigate to={"/login"}/>
